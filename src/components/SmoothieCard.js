a660cf7cbd825c5b18de014a7e201d48971a4768
Mohammed Kharoda <kharodawalam@gmail.com>
2023-06-20 17:27:27 +0530

    inital commit

---
+++ src/components/SmoothieCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import supabase from "../supabase";
+
+
+const SmoothieCard = ({ smoothie , onDeleteHandler}) => {
+  const handleDelete = async (e) => {
+    const {data,error} = await supabase 
+    .from('smoothies')
+    .delete()
+    .eq('id',smoothie.id)
+    .select()
+
+    if(error){
+      console.error(error)
+    }
+    
+    if(data){
+      console.log(data)
+      onDeleteHandler(smoothie.id)
+    }
+  }
+
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      <p>{smoothie.method}</p>
+      <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>delete</i>
+      </div>
+    </div>
+  );
+};
+
+export default SmoothieCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import supabase from "../supabase";
+import { useEffect } from "react";
+import SmoothieCard from "../components/SmoothieCard";
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  const handleDelete = (id) => {
+    setSmoothies(prevSmoothies => {
+      return prevSmoothies.filter(smoothie => smoothie.id !== id);
+    })
+  }
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+
+      if (error) {
+        setFetchError("Could not fetch smoothies", error);
+        setSmoothies(null);
+      }
+      if (data) {
+        setFetchError(null);
+        setSmoothies(data);
+      } 
+    };
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothies) => (
+              <SmoothieCard key={smoothies.id} smoothie={smoothies} onDeleteHandler={handleDelete} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
